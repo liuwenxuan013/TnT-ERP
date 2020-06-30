3855264f5c3a1eec4c03de338feca213eb996b50
Wenxuan Liu <wliu122@myseneca.ca>
2020-06-30 13:44:32 -0400

    comit

---
+++ tnt-erp/build/precache-manifest.007eef0845c38167c59aa88406e7dd27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c3e970c982b87d9ba6d88a7feef591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919a84f0f7a66d3be6d7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2cd6cd00d7d802be5bef",
+    "url": "/static/js/2.63d389ef.chunk.js"
+  },
+  {
+    "revision": "abf03617803901bc022560d39b5885ef",
+    "url": "/static/js/2.63d389ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919a84f0f7a66d3be6d7",
+    "url": "/static/js/main.7b21b8a4.chunk.js"
+  },
+  {
+    "revision": "d42b3f6697f30a3180e3",
+    "url": "/static/js/runtime-main.9a8f78ab.js"
+  }
+]);
